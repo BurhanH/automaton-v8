fb4585d949bc6200d3a4e917776a50ec2af2998e
BurhanH <urazalinov@gmail.com>
2018-10-18 19:24:41 -0500

    Corrected 3

--- src/test/java/com/automaton/selenium/InitialTest.java
+++ src/test/java/com/automaton/selenium/InitialTest.java
@@ -2,39 +2,24 @@ package com.automaton.selenium;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 
-public class InitialTest {
+class InitialTest {
 
     @Test
-    public void testOne(){
+    void testOne(){
         // Simple test. Expecting True
-        assertTrue(new String("Java is awesome!").equals("Java is awesome!"));
+        String text = "Java is awesome!";
+        String text1 = "Java is awesome!";
+        assertEquals(text, text1);
     }
 
     @Test
-    public void testTwo(){
+    void testTwo(){
         // Simple test. Expecting False
-        assertFalse(new String("Java is interesting!") == "Java is interesting!");
-    }
-
-    @Test
-    public void testThree(){
-        // Expecting True
-        assertTrue("Java!" == "Java!");
-    }
-
-    @Test
-    public void testFour(){
-        // Expecting False
-        assertFalse(new String("Java programming language") == new String("Java programming language"));
-    }
-
-    @Test
-    public void testFive(){
-        // Expecting True
-        assertTrue(new String("Java is awesome!").intern() == "Java is awesome!");
+        String text = "Java is awesome!";
+        String text1 = "Java";
+        assertNotEquals(text, text1);
     }
 }
